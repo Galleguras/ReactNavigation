801e0a0f9ae4c2b62686f11802ae8c33c88aac57
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2020-12-07 18:43:28 +0100

    useefect

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
  const [cont,setCont] = useState(0)
- console.log(`primero`,cont)
+ const [loading,setLoading] = useState(true)
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  },[])
+  console.log(`primero`,cont)
+
   return (
     
     <View style={styles.container}>
-      <Text onPress={()=>setCont(cont+1)}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text} onPress={()=>setCont(cont+1)}>{loading?`Cargando`:`Open up App.js to start working on your app! ${cont}`}</Text>
 
     </View>
   );
